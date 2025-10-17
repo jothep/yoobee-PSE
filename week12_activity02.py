77ddd4de8025f6e0b75c5e3043e87dbc9a5e4fa6
Xiang <shelldry325@gmail.com>
2025-10-18 09:35:41 +1300

    week12 activity2

---
+++ week12_activity02.py
@@ -0,0 +1,52 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    html_content = '''
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Input the pic URL here.</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Pic checking</h1>
+            <form action="/show_image" method="GET">
+                <input type="url" name="image_url" placeholder="Pls paste the URL here" required>
+                <button type="submit">Show the picture</button>
+            </form>
+        </div>
+    </body>
+    </html>
+    '''
+    return html_content
+
+@app.route("/show_image")
+def show_image():
+    image_url = request.args.get('image_url', '')
+
+    if image_url.startswith('http://') or image_url.startswith('https://'):
+        image_display_html = '<img src="{}" alt="The picture which user gived" width="600">'.format(image_url)
+    else:
+        image_display_html = '<p>No useful image URL provided. Please make sure it starts with http:// or https://</p>'
+
+    html_content = f'''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>The picture</title>
+    </head>
+    <body>
+        {image_display_html}
+        <hr>
+        <a href="/">Return to index</a>
+    </body>
+    </html>
+    '''
+    return html_content
+
+if __name__ == "__main__":
+    app.run(debug=True)
