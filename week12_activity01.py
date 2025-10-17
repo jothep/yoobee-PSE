ee2436b1d9df4945408a3fef955637352107b1a9
Xiang <shelldry325@gmail.com>
2025-10-18 08:39:28 +1300

    change for activity1.1

--- week12_activity01.py
+++ week12_activity01.py
@@ -2,12 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/username/Lili")
 def hello_flask():
     html_content = '''
     <p style="
         font-size: 72px;">
-        Hello,<br>Flask!
+        Lili is learning Flask!
     </p>
     '''
     return html_content
